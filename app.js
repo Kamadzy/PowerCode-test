2724528dd47edc3a21d88e45c0d9e2479e6b1d3d
Kirill <kam4513@gmail.com>
2016-10-08 02:57:48 +0300

    add saving data to localstorage

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-/**
- * Created by kamadzy on 06.10.2016.
- */
+function saveData(){
+
+    var tasks = localStorage['dLine'] ? JSON.parse(localStorage.dLine) : [];
+
+    var checkBox = $('input[name="optionsRadios"]:checked').val();
+    /*localStorage.setItem("cBox", checkBox);*/
+
+    var textArea = $('#textArea').val();
+    /*localStorage.setItem("tArea", textArea);*/
+
+    var deadLine = $('#datetimepicker').val();
+
+    var task = {status:checkBox,text:textArea,date:deadLine};
+    tasks.push(task);
+    localStorage.setItem("dLine", JSON.stringify(tasks));
+
+    return false;
+}
