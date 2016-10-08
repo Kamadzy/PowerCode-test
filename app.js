e3412fcf1e5a8a0ddc0ac8c78ccff1de656ee676
Kirill <kam4513@gmail.com>
2016-10-08 16:44:02 +0300

    fixed sorter

--- app.js
+++ app.js
@@ -37,7 +37,36 @@ $(document).ready(function(){
         saveData();
     });
     openData();
-    $(".table").tablesorter();
+    $.tablesorter.addParser({
+        id: "deadLine",
+        is: function(s) {
+            //return false;
+            //use the above line if you don't want table sorter to auto detected this parser
+            //21/04/2010 03:54 is the used date/time format
+            return /\d{1,2}\.\d{1,2}\.\d{1,4} \d{1,2}:\d{1,2}:\d{1,2}/.test(s);
 
-    
+        },
+        format: function(s) {
+            console.log(s);
+            s = s.replace(/\-/g," ");
+            s = s.replace(/:/g," ");
+            s = s.replace(/\./g," ");
+            s = s.replace(/\//g," ");
+            s = s.split(" ");
+            console.log(s);
+            return $.tablesorter.formatFloat(new Date(s[2], s[1]-1, s[0], s[3], s[4]).getTime());
+        },
+        type: "numeric"} );
+
+
+        $(".table").tablesorter({
+            headers: {
+                3: {
+                    sorter:'deadLine'
+                }
+            }
+        });
+
+
+
 });
