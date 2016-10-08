5ec59a770abce546fd16a137e7d52ab505b4c81a
Kirill <kam4513@gmail.com>
2016-10-08 16:05:34 +0300

    add sorter and data to table

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-function saveData(){
-
+$(document).ready(function(){
     var tasks = localStorage['dLine'] ? JSON.parse(localStorage.dLine) : [];
+    function openData(){
+        $("#tableBody").empty();
+        for (var i=0; i<tasks.length; i++){
+            $( "#tableBody" ).append( "<tr> <td>"+tasks[i].status+"</td> <td>"+tasks[i].text+"</td> <td>"+tasks[i].date+"</td> <td><button data-id=\""+i+"\" class=\"deleteButton\">Delete Item</button></td> </tr>" );
+        }
+    }
 
-    var checkBox = $('input[name="optionsRadios"]:checked').val();
-    /*localStorage.setItem("cBox", checkBox);*/
+    function saveData(){
 
-    var textArea = $('#textArea').val();
-    /*localStorage.setItem("tArea", textArea);*/
+        var checkBox = $('input[name="optionsRadios"]:checked').val();
 
-    var deadLine = $('#datetimepicker').val();
+        var textArea = $('#textArea').val();
 
-    var task = {status:checkBox,text:textArea,date:deadLine};
-    tasks.push(task);
-    localStorage.setItem("dLine", JSON.stringify(tasks));
+        var deadLine = $('#datetimepicker').val();
 
-    return false;
-}
+        var task = {status:checkBox,text:textArea,date:deadLine};
+        tasks.push(task);
+        localStorage.setItem("dLine", JSON.stringify(tasks));
+
+        openData();
+    }
+    function deleteItem(id){
+        for (var i=0; i<tasks.length; i++){
+          if(i==parseInt(id)){
+             tasks.splice(i,1);
+          }
+        }
+        localStorage.setItem("dLine", JSON.stringify(tasks));
+        openData();
+    }
+    $("#tableBody").on("click",".deleteButton", function(){
+        deleteItem($(this).data("id"));
+    });
+    $("#saveButton").click(function(){
+        saveData();
+    });
+    openData();
+    $(".table").tablesorter();
+
+    
+});
